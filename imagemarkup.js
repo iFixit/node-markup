ce8044e3dd630ea9497aba0429d4544421d076d7
Cedric Wienold <cedric@ifixit.com>
2013-02-26 17:07:12 -0800

    Remove cloneextend from use
    cloneextend is a Node.js-only tool. While the code itself looks easy
    enough to make work on the web, my needs are so small that it's more
    worth it to use a custom clone function.

--- imagemarkup.js
+++ imagemarkup.js
@@ -2,6 +2,58 @@ var FS = require('fs');
 var GM = require('gm');
 var Fabric = require('fabric').fabric;
 var Cloner = require('cloneextend');
+//var clone = Cloner.clone;
+function clone(src) {
+  function mixin(dest, source, copyFunc) {
+    var name, s, i, empty = {};
+    for(name in source){
+      // the (!(name in empty) || empty[name] !== s) condition avoids copying properties in "source"
+      // inherited from Object.prototype.   For example, if dest has a custom toString() method,
+      // don't overwrite it with the toString() method that source inherited from Object.prototype
+      s = source[name];
+      if(!(name in dest) || (dest[name] !== s && (!(name in empty) || empty[name] !== s))){
+        dest[name] = copyFunc ? copyFunc(s) : s;
+      }
+    }
+    return dest;
+  }
+
+  if(!src || typeof src != "object" || Object.prototype.toString.call(src) === "[object Function]"){
+    // null, undefined, any non-object, or function
+    return src;  // anything
+  }
+  if(src.nodeType && "cloneNode" in src){
+    // DOM Node
+    return src.cloneNode(true); // Node
+  }
+  if(src instanceof Date){
+    // Date
+    return new Date(src.getTime());  // Date
+  }
+  if(src instanceof RegExp){
+    // RegExp
+    return new RegExp(src);   // RegExp
+  }
+  var r, i, l;
+  if(src instanceof Array){
+    // array
+    r = [];
+    for(i = 0, l = src.length; i < l; ++i){
+      if(i in src){
+        r.push(clone(src[i]));
+      }
+    }
+    // we don't clone functions for performance reasons
+    //    }else if(d.isFunction(src)){
+    //      // function
+    //      r = function(){ return src.apply(this, arguments); };
+}else{
+  // generic objects
+  r = src.constructor ? new src.constructor() : {};
+}
+return mixin(r, src, clone);
+
+}
 var argv = require('optimist').argv;
 
 var colorValues = {
@@ -310,16 +362,15 @@ function drawRectangle(json, canvas, shape, imageOffset) {
   rect['top'] = rect['top'] + rect['height'] / 2 + rect['strokeWidth'] / 2;
   rect['left'] = rect['left'] + rect['width'] / 2 + rect['strokeWidth'] / 2;
 
-  var rectBorder = Cloner.clone(rect);
+  var rectBorder = clone(rect);
   rectBorder['width'] = rectBorder['width'] + rect['strokeWidth'] + whiteStroke;
   rectBorder['height'] = rectBorder['height'] + rect['strokeWidth'] + whiteStroke;
   rectBorder['rx'] = rect['strokeWidth'] - whiteStroke;
   rectBorder['ry'] = rect['strokeWidth'] - whiteStroke;
-  console.log("rx = " + rectBorder['rx']);
   rectBorder['strokeWidth'] = whiteStroke;
   rectBorder['stroke'] = 'white';
 
-  var rectInline = Cloner.clone(rect);
+  var rectInline = clone(rect);
   rectInline['width'] = rectInline['width'] - rect['strokeWidth'] + whiteStroke;
   rectInline['height'] = rectInline['height'] - rect['strokeWidth'] + whiteStroke;
   rectInline['rx'] = Math.max(shape['stroke'] - rectBorder['rx'],4);
@@ -349,12 +400,12 @@ function drawCircle(json, canvas, shape, imageOffset) {
     stroke: colorValues[shape['color']],
     fill: 'transparent'
   };
-  var circleBorder = Cloner.clone(circle);
+  var circleBorder = clone(circle);
   circleBorder['radius'] = circle['radius'] + circle['strokeWidth'] / 2;
   circleBorder['strokeWidth'] = whiteStroke;
   circleBorder['stroke'] = 'white';
 
-  var circleInline = Cloner.clone(circleBorder);
+  var circleInline = clone(circleBorder);
   circleInline['radius'] = circle['radius'] - circle['strokeWidth'] / 2;
 
   if (shadows == true) {
