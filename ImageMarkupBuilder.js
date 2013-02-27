321febd0bc867404944d50e9e7aacb0d2a8e3aab
Cedric Wienold <cedric@ifixit.com>
2013-02-26 17:08:44 -0800

    Move functions into module
    In an effort to start allowing code to be shared between frontend and
    backend, move functions into a module and create a CLI script.

---
+++ ImageMarkupBuilder.js
@@ -0,0 +1,318 @@
+var isNode = typeof module != 'undefined' && module.exports;
+
+
+/**
+ * Expects a Fabric.js Canvas
+ */
+function ImageMarkupBuilder(canvas) {
+  if (isNode) {
+    var FS = require('fs');
+    var Fabric = require('fabric').fabric;
+  } else {
+    var FS = fs;
+    var Fabric = fabric;
+  }
+
+  var colorValues = {
+    'red': 'rgb(193,40,11)',
+    'orange': 'rgb(355,144,36)',
+    'yellow': 'rgb(243,224,14)',
+    'green': 'rgb(22,220,129)',
+    'blue': 'rgb(35,67,232)',
+    'violet': 'rgb(220,84,183)',
+    'black': 'rgb(0,0,0)'
+  };
+
+  var imageOffset;
+
+  function clone(obj) {
+    var newobj = {};
+    for (property in obj) {
+      if (typeof obj[property] == 'object') {
+        newobj[property] = clone(property);
+      } else {
+        newobj[property] = obj[property];
+      }
+    }
+    return newobj;
+  }
+
+  /**
+   * Cycles through an object and changes all numeric fields to ints
+   * where necessary. 'context' is used for exception reporting and can be
+   * left unset upon invocation.
+   */
+  function cleanJSON(json, context) {
+    if (!context)
+      context = "root";
+
+    for (property in json) {
+      if (typeof(json[property]) == 'object') {
+        cleanJSON(json[property], context + '.' + property);
+      }
+      else if (property == 'x' || property == 'y' ||
+               property == 'width' || property == 'height' ||
+               property == 'radius') {
+        if (typeof(json[property]) == 'string') {
+          json[property] = parseInt(json[property]);
+          if (isNaN(json[property])) {
+            var msg = "In '" + context + "': property '" + property + "' is not a number.";
+            throw msg;
+          }
+        }
+      }
+    }
+  }
+
+  function applyBackground(json, canvas, callback) {
+    FS.readFile(json['sourceFile'], function (err, blob) {
+      if (err) throw err;
+
+      var dimensions = json['dimensions'];
+      var finalDimensions = json['finalDimensions'];
+      img = {'width': dimensions['width'], 'height': dimensions['height'], 'src':blob};
+
+      Fabric.Image.fromObject(img, function(fimg) {
+        top = img.height/2 - imageOffset['y'];
+        if (top % 1 != 0) {
+          top -= 0.5;
+        }
+        left = img.width / 2 - imageOffset['x'];
+        if (left % 1 != 0) {
+          left -= 0.5;
+        }
+
+        canvas.add(fimg.set('top', top).set('left', left));
+
+        applyMarkup(json, canvas, callback);
+      });
+    });
+  }
+
+  function applyMarkup(json, canvas, callback) {
+    var size = json['dimensions'];
+
+    for (instruction in json['instructions']) {
+      switch (instruction) {
+        case 'draw':
+          json['instructions']['draw'].forEach(function (e) {
+            for (shapeName in e) {
+              shape = e[shapeName];
+              shape['shapeName'] = shapeName;
+
+              switch (shapeName) {
+                case 'rectangle':
+                  drawRectangle(json, canvas, shape, imageOffset);
+                  break;
+                case 'circle':
+                  drawCircle(json, canvas,shape, imageOffset);
+                  break;
+                default:
+                  console.error('Unsupported Shape: ' + shapeName);
+              }
+            }
+          });
+          break;
+        case 'crop':
+          //もう処理していたから無視する
+          break;
+        default:
+          console.error('Unsupported Instruction: ' + instruction);
+      }
+    }
+
+    writeCanvas(json, canvas, callback);
+  }
+
+  function writeCanvas(json, canvas, callback) {
+    canvas.renderAll();
+    var outstream = FS.createWriteStream(json['destinationFile']),
+        stream = canvas.createJPEGStream({
+          quality: 93
+        });
+
+    stream.on('data', function(chunk) {
+      outstream.write(chunk);
+    });
+    stream.on('end', function () {
+      callback(canvas);
+    });
+  }
+
+  function drawRectangle(json, canvas, shape, imageOffset) {
+    shape['stroke'] = Math.max(Math.round(json['finalDimensions']['width'] / 300 * 2), 2);
+    whiteStroke = 2;
+
+    var rect = {
+      shapeName: shape['shapeName'],
+      left: shape['from']['x']-imageOffset['x'],
+      top: shape['from']['y']-imageOffset['y'],
+      width: shape['size']['width'],
+      height: shape['size']['height'],
+      rx: 1,
+      ry: 1,
+      strokeWidth: shape['stroke'],
+      stroke: colorValues[shape['color']],
+      fill: 'transparent'
+    }
+
+    //Fabric調整
+    rect['top'] = rect['top'] + rect['height'] / 2 + rect['strokeWidth'] / 2;
+    rect['left'] = rect['left'] + rect['width'] / 2 + rect['strokeWidth'] / 2;
+
+    var rectBorder = clone(rect);
+    rectBorder['width'] = rectBorder['width'] + rect['strokeWidth'] + whiteStroke;
+    rectBorder['height'] = rectBorder['height'] + rect['strokeWidth'] + whiteStroke;
+    rectBorder['rx'] = rect['strokeWidth'] - whiteStroke;
+    rectBorder['ry'] = rect['strokeWidth'] - whiteStroke;
+    rectBorder['strokeWidth'] = whiteStroke;
+    rectBorder['stroke'] = 'white';
+
+    var rectInline = clone(rect);
+    rectInline['width'] = rectInline['width'] - rect['strokeWidth'] + whiteStroke;
+    rectInline['height'] = rectInline['height'] - rect['strokeWidth'] + whiteStroke;
+    rectInline['rx'] = Math.max(shape['stroke'] - rectBorder['rx'],4);
+    rectInline['ry'] = Math.max(shape['stroke'] - rectBorder['ry'],4);
+    rectInline['strokeWidth'] = whiteStroke;
+    rectInline['stroke'] = 'white';
+
+    if (shadows == true) {
+      drawShadow(canvas, rect, shadowStep);
+    }
+
+    canvas.add(new Fabric.Rect(rect));
+    canvas.add(new Fabric.Rect(rectBorder));
+    canvas.add(new Fabric.Rect(rectInline));
+  }
+
+  function drawCircle(json, canvas, shape, imageOffset) {
+    shape['stroke'] = Math.max(Math.round(json['finalDimensions']['width'] / 300 * 2), 2);
+    whiteStroke = 2;
+
+    var circle = {
+      shapeName: shape['shapeName'],
+      left: shape['from']['x']-imageOffset['x'],
+      top: shape['from']['y']-imageOffset['y'],
+      radius: shape['radius'],
+      strokeWidth: shape['stroke'],
+      stroke: colorValues[shape['color']],
+      fill: 'transparent'
+    };
+    var circleBorder = clone(circle);
+    circleBorder['radius'] = circle['radius'] + circle['strokeWidth'] / 2;
+    circleBorder['strokeWidth'] = whiteStroke;
+    circleBorder['stroke'] = 'white';
+
+    var circleInline = clone(circleBorder);
+    circleInline['radius'] = circle['radius'] - circle['strokeWidth'] / 2;
+
+    if (shadows == true) {
+      drawShadow(canvas, circle, shadowStep);
+    }
+
+    canvas.add(new Fabric.Circle(circle));
+    canvas.add(new Fabric.Circle(circleBorder));
+    canvas.add(new Fabric.Circle(circleInline));
+  }
+
+  /**
+   * Draw a fuzzy shadow for the shape given.
+   * Take care to draw the shadow before the shape.
+   */
+  function drawShadow(canvas, shape, step) {
+    if (step < 1) {
+      step = 1;
+    }
+    var shadow = Cloner.clone(shape);
+    if (step > shadow['strokeWidth']) {
+      step = shadow['strokeWidth'];
+    }
+
+    var offsetX = 8;
+    var offsetY = offsetX;
+
+    var shadow = Cloner.clone(shape);
+    shadow['left'] += offsetX;
+    shadow['top'] += offsetY;
+    shadow['rx'] = 5;
+    shadow['ry'] = 5;
+    shadow['stroke'] = 'rgba(0,0,0,0.5)';
+    shadow['strokeWidth'] = shadow['strokeWidth'] / step;
+    var stepWidth = shadow['strokeWidth'];
+
+    //Empirically-derived pixel tweaks to line up shadow sizes with their parents
+    var circleTweak = 0.6875;
+    var rectangleTweak = 1.3125;
+
+    //Adjust shadow outlines to outer edge, to work towards inside later.
+    switch (shape['shapeName']) {
+      case 'circle':
+        shadow['radius'] += shape['strokeWidth'] * circleTweak;
+        shadow['strokeWidth'] *= 2;
+        break;
+      case 'rectangle':
+        shadow['width'] += shape['strokeWidth'] * rectangleTweak;
+        shadow['height'] += shape['strokeWidth'] * rectangleTweak;
+        shadow['strokeWidth'] *= 2;
+        break;
+      default:
+        console.error('実装されていない機能：' + shape['shapeName']);
+        return;
+    }
+
+    var alpha;
+    for (var i = 0; i < step; ++i) {
+      if (i < (step / 2)) {
+        alpha = (((i + 1) * 2) / (step));
+      }
+      else if (i == step / 2) {
+        //Math ain't working to my likings
+        alpha = 1;
+      }
+      else {
+        alpha = (((step - (i + 1)) * 2) / (step));
+      }
+
+      shadow['stroke'] = 'rgba(0,0,0,' + alpha + ')';
+
+      switch (shape['shapeName']) {
+        case 'circle':
+          canvas.add(new Fabric.Circle(shadow));
+          shadow['radius'] = shadow['radius'] - stepWidth * 2 * 0.8;
+          break;
+        case 'rectangle':
+          canvas.add(new Fabric.Rect(shadow));
+          shadow['width'] = shadow['width'] - stepWidth * 4 * 0.8;
+          shadow['height'] = shadow['height'] - stepWidth * 4 * 0.8;
+          break;
+        default:
+          console.error('実装されてない機能：' + shape['shapeName']);
+          return;
+      }
+    }
+  }
+
+  return {
+    processJSON: function processJSON(json, callback) {
+      //Make sure not to render every addition on server end
+      canvas.renderOnAddition = !isNode;
+
+      cleanJSON(json);
+
+      var imagePath = json['sourceFile'];
+
+      var crop = json['instructions']['crop'];
+      imageOffset = (typeof crop != "undefined") ?
+       {'x':crop['from']['x'],
+        'y':crop['from']['y']} :
+       {'x':0,'y':0};
+
+      applyBackground(json, canvas, callback);
+    }
+  };
+}
+
+if (isNode)
+  module.exports = ImageMarkupBuilder;
+else
+  var createImageFromMarkup = ImageMarkupBuilder;
---
+++ ImageMarkupCall.js
@@ -0,0 +1,234 @@
+var ImageMarkupBuilder = require('./ImageMarkupBuilder');
+var GM = require('gm');
+var Fabric = require('fabric').fabric;
+var argv = require('optimist').argv;
+
+var colorValues = {
+  'red': 'rgb(193,40,11)',
+  'orange': 'rgb(355,144,36)',
+  'yellow': 'rgb(243,224,14)',
+  'green': 'rgb(22,220,129)',
+  'blue': 'rgb(35,67,232)',
+  'violet': 'rgb(220,84,183)',
+  'black': 'rgb(0,0,0)'
+};
+
+function usage(err) {
+  var filename = __filename.replace(/^.*[\\\/]/, '');
+  console.log('Example Usage:');
+  console.log('node ' + filename + ' [--help|-h] - Show this information');
+  console.log('node ' + filename + ' --input infile.jpg --output outfile.jpg --markup \'[markup_string]\'');
+  console.log('node ' + filename + ' --json \'[json_string]\'');
+  //TODO: "See README file for specification"
+  if (err)
+    process.exit(err);
+  else
+    process.exit(0);
+}
+
+function processArgs() {
+  if (argv.help || argv.h) {
+    usage();
+  }
+
+  shadows = 
+   typeof(argv.shadows) == 'undefined'
+    ? false
+    : argv.shadows == 'true' || argv.shadows == true;
+
+  if (shadows) {
+    if (argv.step) {
+      shadowStep = parseInt(argv.step);
+    }
+  }
+
+  var json;
+  if (argv.json && argv.markup) {
+    console.error('Invalid usage.');
+    usage(-1);
+  } else if (argv.json) {
+    var json = JSON.parse(argv.json);
+    processJSON(json);
+  } else if (argv.markup) {
+    if (!argv.input || !argv.output) {
+      console.error('Invalid usage. Input or output path missing.');
+      usage(-1);
+    }
+
+    convertMarkupToJSON(processJSON, argv.markup, argv.input, argv.output);
+  } else {
+    console.error('Invalid uage.');
+    usage(-1);
+  }
+}
+
+function convertMarkupToJSON(callback, markup, infile, outfile) {
+  var json = {};
+
+  GM(infile).size(function (err, size) {
+    if (err) throw err;
+
+    json['dimensions'] = size;
+    json['finalDimensions'] = size;
+
+    json['instructions'] = {};
+
+    var instructions = markup.split(";");
+    for (var i = 0; i < instructions.length; ++i) {
+      if (instructions[i] == '') continue;
+
+      var args = instructions[i].split(",");
+      var command = args[0];
+      switch (command) {
+        case 'crop':
+          var position = args[1].split("x");
+          position[0] = parseInt(position[0]);
+          position[1] = parseInt(position[1]);
+          var from = {
+            'x': position[0],
+            'y': position[1]
+          };
+
+          var dimensions = args[2].split("x");
+          dimensions[0] = parseInt(dimensions[0]);
+          dimensions[1] = parseInt(dimensions[1]);
+          var size = {
+            'width': dimensions[0],
+            'height': dimensions[1]
+          };
+
+          var crop = {};
+          crop['from'] = from;
+          crop['size'] = size;
+
+          json['instructions']['crop'] = crop;
+          json['finalDimensions'] = crop['size'];
+          break;
+        case 'circle':
+          if (!json['instructions']['draw']) json['instructions']['draw'] = new Array();
+
+          var position = args[1].split("x");
+          position[0] = parseInt(position[0]);
+          position[1] = parseInt(position[1]);
+          var from = {
+            'x': position[0],
+            'y': position[1]
+          };
+
+          var radius = parseInt(args[2]);
+          var color = args[3];
+
+          var circle = {};
+          circle['from'] = from;
+          circle['radius'] = radius;
+          circle['color'] = color;
+
+          json['instructions']['draw'].push({'circle': circle});
+          break;
+        case 'rectangle':
+          if (!json['instructions']['draw']) json['instructions']['draw'] = new Array();
+
+          var position = args[1].split("x");
+          position[0] = parseInt(position[0]);
+          position[1] = parseInt(position[1]);
+          var from = {
+            'x': position[0],
+            'y': position[1]
+          };
+
+          var dimensions = args[2].split("x");
+          dimensions[0] = parseInt(dimensions[0]);
+          dimensions[1] = parseInt(dimensions[1]);
+          var size = {
+            'width': dimensions[0],
+            'height': dimensions[1]
+          };
+
+          var color = args[3];
+
+          var rectangle = {
+            'from': from,
+            'size': size,
+            'color': color
+          };
+
+          json['instructions']['draw'].push({'rectangle': rectangle});
+          break;
+        default:
+      }
+    }
+
+    json['sourceFile'] = infile;
+    json['destinationFile'] = outfile;
+
+    callback(json);
+  });
+}
+
+function processJSON(json) {
+  cleanJSON(json);
+
+  var size = json['finalDimensions'];
+  var canvas = Fabric.createCanvasForNode(size['width'], size['height']);
+  var builder = ImageMarkupBuilder(canvas);
+
+  builder.processJSON(json, proceed);
+}
+
+function proceed(canvas) {
+  console.log("Finished writing");
+}
+
+/**
+ * Pretty-prints a JSON object to the console. The first invocation not set the
+ * 'level' parameter.
+ */
+function printJSON(json, level) {
+  String.prototype.repeat = function (num) {
+    return new Array(num+1).join(this);
+  }
+  if (!level) {
+    console.log("{");
+    printJSON(json, 1);
+    console.log("}");
+  } else {
+    for (property in json) {
+      console.log("\t".repeat(level) + property + ": " + json[property] + " [" + typeof(json[property]) + "]");
+      if (typeof(json[property]) == 'object') {
+        console.log("\t".repeat(level) + "{");
+        printJSON(json[property], level+1);
+        console.log("\t".repeat(level) + "}");
+      }
+    }
+  }
+}
+
+/**
+ * Cycles through an object and changes all numeric fields to ints
+ * where necessary. 'context' is used for exception reporting and can be
+ * left unset upon invocation.
+ */
+function cleanJSON(json, context) {
+  if (!context)
+    context = "root";
+
+  for (property in json) {
+
+    if (typeof(json[property]) == 'object') {
+      cleanJSON(json[property], context + '.' + property);
+    }
+    else if (property == 'x' || property == 'y' ||
+             property == 'width' || property == 'height' ||
+             property == 'radius') {
+      if (typeof(json[property]) == 'string') {
+        json[property] = parseInt(json[property]);
+        if (isNaN(json[property])) {
+          var msg = "In '" + context + "': property '" + property + "' is not a number.";
+          throw msg;
+        }
+      }
+    }
+  }
+}
+
+processArgs();
