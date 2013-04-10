e69962de56cb5eaa12f908a74edca4c225dcee9c
Cedric Wienold <cedric@ifixit.com>
2013-04-10 16:37:17 -0700

    Add automated 'gm compare' invocation

--- run-tests.py
+++ run-tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import os;
+import subprocess;
 import glob;
 import re as regex;
 
@@ -10,6 +11,14 @@ def readMarkupFile(markupFilename):
    f = open(markupFilename, 'r');
    markup = f.read();
    return markup;
+
+def compareOutputs(oracleFilename, destinationFilename):
+   metric = "mae";
+   cmd = ["gm","compare","-metric",metric,oracleFilename,destinationFilename];
+
+   proc = subprocess.Popen(cmd, stdout=subprocess.PIPE);
+   (out, err) = proc.communicate();
+   print out;
 
 def runNode(sourceFilename, destinationFilename, markupFilename):
    markup = readMarkupFile(markupFilename);
@@ -25,12 +34,17 @@ def runNode(sourceFilename, destinationFilename, markupFilename):
    else:
       print(sourceFilename + ' -> ' + destinationFilename);
 
+   return ret == 0;
+
 for filename in os.listdir(testDirectory):
    if filename.endswith(".markup"):
-         markupFilename = testDirectory + filename;
-         basename = regex.sub(r'(.+)\.markup', r'\1', filename);
-         sourceFilename = testDirectory + basename + '.source.jpg';
-         oracle = testDirectory + basename + '.node.oracle.jpg';
-         testFilename = testDirectory + basename + '.node.test.jpg';
+      markupFilename = testDirectory + filename;
+      basename = regex.sub(r'(.+)\.markup', r'\1', filename);
+      sourceFilename = testDirectory + basename + '.source.jpg';
+      oracleFilename = testDirectory + basename + '.node.oracle.jpg';
+      testFilename = testDirectory + basename + '.node.test.jpg';
 
-         runNode(sourceFilename, testFilename, markupFilename);
+      success = runNode(sourceFilename, testFilename, markupFilename);
+
+      if success:
+         compareOutputs(oracleFilename, testFilename);
