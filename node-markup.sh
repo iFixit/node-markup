131f331c2127a9a696104caeb720a7e5b9fed642
Marc Zych <marczych@gmail.com>
2015-03-13 18:55:36 -0700

    node-markup.sh: Update node_modules path
    The path changed when upgrading servers so it needs to be changed here.

--- node-markup.sh
+++ node-markup.sh
@@ -10,5 +10,5 @@ cd $DIR
 # NODE_PATH is for finding node modules in different locations. If you did not
 # install modules to locations other than this folder, you may comment this
 # line out
-export NODE_PATH=/usr/local/share/node-markup/node_modules:/usr/local/lib/node_modules
+export NODE_PATH=/usr/local/share/node-markup/node_modules:/usr/lib/node_modules
 node $DIR/ImageMarkupCall.js $@
