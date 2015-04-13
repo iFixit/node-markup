5272cc291153ce33777471d1b4f90cf5217f3840
Daniel Beardsley <daniel@ifixit.com>
2015-04-13 11:56:43 -0700

    Lines: move sizeByMouse to two_point_interactivity
    Cause it deals with two-point interaction :-)

--- shape_line.js
+++ shape_line.js
@@ -15,33 +15,11 @@ var Line = Fabric.util.createClass(Fabric.Line, {
    shapeName: 'line',
    sizeLimits: [0.04, 0.4],
 
-   /**
-    * Resizes this line to make the most sense given the two points (they
-    * will determine two opposite corners.
-    */
-   sizeByMousePos: function(x1, y1, x2, y2) {
-      var xd = x2 - x1;
-      var yd = y2 - y1;
-      var rad = Math.sqrt(xd * xd + yd * yd);
-      if (!rad) {
-         xd = yd = this._limitDimension(rad);
-         var ratio = 1;
-      } else {
-         var ratio = this._limitDimension(rad) / rad ;
-      }
-      this.x1 = x1;
-      this.y1 = y1;
-      this.x2 = x1 + ratio * xd;
-      this.y2 = y1 + ratio * yd;
-      this._setWidthHeight();
-      this.setCoords();
-   },
-
    render: function(ctx) {
       this._fixAndRestoreSubPixelPositioning(function() {
          this.callSuper('render', ctx, /* noTransform */ false);
       });
-   }
+   },
 });
 
 var proto = Line.prototype;
