e6b0e995a6fb0b78d9d4a49dfb126bb05e6ee317
Daryl Metzler <daryl@ifixit.com>
2022-07-18 17:28:49 -0700

    Prettier the eslint config
    What irony.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,14 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    es2021: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
