55872086636c4b0af9f93d2647b8eda17a022817
Daryl Metzler <daryl@ifixit.com>
2022-08-08 15:31:51 -0700

    Generalize markup_to_json.test.js
    Lets check all of the markup test cases we have available under
    `./test/` and write down the expected instructions. This can serve as
    good test to ensure a rewrite of the parse works as we expect.

--- src/markup_to_json.test.js
+++ src/markup_to_json.test.js
@@ -1,49 +1,32 @@
 const fs = require("fs");
 const assert = require("node:assert").strict;
 
+const TestCases = [
+  "000066",
+  "00CC00",
+  "66CCFF",
+  "9900CC",
+  "993333",
+  "lines",
+  "simple-lines",
+];
+
 const convertMarkupToJSON = require("./markup_to_json");
 
-const expectedJSON = {
-  dimensions: { width: 1200, height: 900 },
-  finalDimensions: { width: 1032, height: 774 },
-  instructions: {
-    crop: {
-      from: { x: 168, y: 71 },
-      size: { width: 1032, height: 774 },
-    },
-    draw: [
-      { circle: { from: { x: 548, y: 671 }, radius: 21, color: "yellow" } },
-      {
-        rectangle: {
-          from: { x: 287, y: 484 },
-          size: { width: 28, height: 28 },
-          color: "blue",
-        },
-      },
-      { circle: { from: { x: 980, y: 467 }, radius: 113, color: "red" } },
-      {
-        rectangle: {
-          from: { x: 196, y: 200 },
-          size: { width: 404, height: 28 },
-          color: "black",
-        },
-      },
-    ],
-  },
-  sourceFile: "./test/993333.source.jpg",
-  destinationFile: "./literally.anywhere",
-};
-
 describe("markup_to_jsonJSON", () => {
-  test('Markup converts to expected JSON', async () => {
-    const testMarkup = fs.readFileSync("./test/993333.markup", "utf8");
-    const testJSON = await convertMarkupToJSON(
-      testMarkup,
-      "./test/993333.source.jpg",
-      "./literally.anywhere"
-    );
+  test("Markup converts to expected JSON", async () => {
+    TestCases.forEach(async (testcase) => {
+      const testMarkup = fs.readFileSync(`./test/${testcase}.markup`, "utf8");
+      const expectedJSON = JSON.parse(
+        fs.readFileSync(`./test/${testcase}.json`, "utf8")
+      );
+      const testJSON = await convertMarkupToJSON(
+        testMarkup,
+        `./test/${testcase}.source.jpg`,
+        "./literally.anywhere"
+      );
 
-    expect(testJSON).toEqual(expectedJSON);
-  })
+      expect(testJSON).toEqual(expectedJSON);
+    });
+  });
 });
-
